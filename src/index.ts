32f189910d883c14b4c59482b75de2eef7babebc
JahirMM <jahirmachuca45@gmail.com>
2025-10-24 17:24:52 -0300

    init: primer commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.get("/", (_, res) => {
+  res.send("API funcionando 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
